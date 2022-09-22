e8492decc4900bdef17000ea56ce44b0808396d7
gavuolo <gabs.vuolo@gmail.com>
2022-09-21 22:08:08 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import arrayPalavras from './palavras'
+import palavras from './palavras'
+
+//imagens
+import forca from "./image/forca0.png"
+import erro01 from "./image/forca1.png"
 
 export default function App() {
     return (
     <div>
-        {arrayPalavras[0]}
+        <h1>Jogo da forca</h1>
+        <div className='topbar'>
+
+            <div className="forca">
+                <img src={forca} alt="" />
+            </div>
+
+            <div className="palavra">
+                <button className='escolher'>Escolher palavra</button>
+                <p>{palavras[1]}</p>
+            </div>
+        </div>
     </div>
     )
 }
