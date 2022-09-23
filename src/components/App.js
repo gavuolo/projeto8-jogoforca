14a25a24e069f0310a27cd209b4fb1281a4456c2
gavuolo <gabs.vuolo@gmail.com>
2022-09-23 15:01:43 -0400

    att

--- src/components/App.js
+++ src/components/App.js
@@ -4,67 +4,49 @@ import palavras from './palavras'
 
 let arrayAleatorio = []
 
+
 export default function App() {
 
-   
+    //começar o jogo
+    const [isStart, setStart] = useState(false)
+    function comecar() {
+        setStart(!isStart)
+    }
+
+    //aleatorizar palavras + array
+    const aleatorio = (palavras[Math.floor(Math.random() * palavras.length)])
+    arrayAleatorio = aleatorio
+    
+
+
+    //estado de clicado da Letra
+    const [clicado, setClicado] = useState([])
+    function clicou(letraClicada) {
+        setClicado([...clicado, letraClicada])
+    }
+
 
     return (
         <div>
             <h1>Jogo da forca</h1>
             <div className='topbar'>
-                <Forca />
-                <Palavra />
+                <div className="forca">
+                    <img src="./image/forca0.png" alt="" />
+                </div>
+                <div className="palavra">
+                    {!isStart ? <button className='escolher' onClick={comecar}>Escolher palavra</button> : <button className='escolher'>Escolher palavra</button>}
+                    {isStart ? <p>{arrayAleatorio}</p> : <p></p>}
+                </div>
             </div>
-            
-            <Letra />
-            <Chute />
+
+            <div className="bottom">
+                {alfabeto.map((l) => <div key={l} className={clicado.includes(l) ? "selecionado" : "botao"} onClick={() => clicou(l)}>{l.toUpperCase()}</div>)}
+            </div>
+            <div className="input">
+                {isStart ? <input type="text" placeholder="Chute a palavra certa" /> : <input type="text" placeholder="Comece o jogo" desable /> }
+                <div className="enviar"><ion-icon name="send"></ion-icon></div>
+            </div>
         </div>
     )
 }
 
-function Palavra() {
-    const [isStart, setStart] = useState(false)
-    function comecar() {
-        setStart(!isStart)
-        
-    }
-    const aleatorio = (palavras[Math.floor(Math.random() * palavras.length)])
-    arrayAleatorio = aleatorio
-    //let escondido = [aleatorio].fill(" _ ")
-   
-    
-    return (
-        <div className="palavra">
-            {!isStart ? <button className='escolher' onClick={comecar}>Escolher palavra</button> : <button className='escolher'>Escolher palavra</button>}
-            {isStart ? <p>{arrayAleatorio}</p> : <p></p>}
-        </div>
-    )
-}
-function Forca() {
-    return (
-        <div className="forca">
-            <img src="./image/forca0.png" alt="" />
-        </div>
-    )
-}
-
-function Letra() {
-    const [clicado, setClicado] = useState([])
-    function clicou(letraClicada) {
-        setClicado([...clicado, letraClicada])
-    }
-    return (
-        <div className="bottom">
-            {alfabeto.map((l) => <div key={l} className={clicado.includes(l) ? "selecionado" : "botao"} onClick={() => clicou(l)}>{l.toUpperCase()}</div>)}
-        </div>
-    )
-}
-
-function Chute() {
-    return (
-        <div className="input">
-            <input type="text" placeholder="Chute a palavra certa" />
-            <div className="enviar"><ion-icon name="send"></ion-icon></div>
-        </div>
-    )
-}
