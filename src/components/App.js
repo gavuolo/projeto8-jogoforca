07116ea35ba0898a828060850c7c12ec4a41254c
gavuolo <gabs.vuolo@gmail.com>
2022-09-26 15:14:07 -0400

    atributos add

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,7 @@ export default function App() {
     const [erros, setErros] = useState(0)
     const [imagem, setImagem] = useState(forca0)
     const [letraCerta, setLetraCerta] = useState([])
+    const [isWin, setWin] = useState(false)
 
 
     function comecar() {
@@ -35,7 +36,14 @@ export default function App() {
         setPalavra(aleatorio)
         //esconder palavra
         arrayLetra = aleatorio.split('')
+
+
         escondido = arrayLetra.map(() => " _ ")
+
+        //console.log(arrayLetra)
+
+        //console.log(escondido)
+        //console.log(arrayLetra)
     }
 
 
@@ -44,25 +52,49 @@ export default function App() {
 
     function clicou(letraClicada) {
 
+        //array de letras já clicadas
         let arrayClicado = clicado.includes(letraClicada) ? clicado : [...clicado, letraClicada];
         setClicado(arrayClicado)
-        console.log(arrayClicado)
+        console.log(isWin)
         console.log(palavra)
-        let ponto = arrayLetra.includes(letraClicada) ? setLetraCerta([...letraCerta, letraClicada]) : setErros(erros + 1)
+        
 
-        //comparaLetra(letraClicada)
+
+
+        comparaLetra(letraClicada)
 
     }
-    //function comparaLetra(letraClicada){
-    //    if(letraClicada === arrayLetra){
-    //       return "acertou"
-    // }
-    //}
+
+    function comparaLetra(letraClicada) {
+
+        //arrayLetra.forEach((indice) =>{
+
+        
+            if (arrayLetra.includes(letraClicada)) {
+                setLetraCerta([...letraCerta, letraClicada])
+                //let nova = letraClicada.normalize("NFD").replace(/[^a-zA-Z\s]/g, "")
+                return(
+                    console.log("acertou")
+                )
+                    
+            } else {
+                setErros(erros + 1)
+                return (
+                    console.log("errou")
+                )
+                } 
+            //})
+        }
+    
+    
+                  
+
 
     //input
     function chutarPalavra() {
+        console.log(isWin)
         if (palavra === chute) {
-            console.log("acertou")
+            setWin(true)
         } else {
             setErros(6)
         }
@@ -83,10 +115,10 @@ export default function App() {
         } else {
             return (
                 <div className="bottom">
-                 {alfabeto.map((l) => 
-                 <button key={l} className={clicado.includes(l) ? "selecionado" : "botao"} onClick={() => clicou(l)}>
-                    {l.toUpperCase()}
-                </button>)}
+                    {alfabeto.map((l) =>
+                        <button data-identifier="letter" key={l} className={clicado.includes(l) ? "selecionado" : "botao"} onClick={() => clicou(l)}>
+                            {l.toUpperCase()}
+                        </button>)}
                 </div>
             )
         }
@@ -95,44 +127,74 @@ export default function App() {
     }
 
     function Forca() {
-        if (erros === 0) {
-            setImagem(forca0)
-        } else if (erros === 1) {
-            setImagem(forca1)
-        } else if (erros === 2) {
-            setImagem(forca2)
-        } else if (erros === 3) {
-            setImagem(forca3)
-        } else if (erros === 4) {
-            setImagem(forca4)
-        } else if (erros === 5) {
-            setImagem(forca5)
-        } else if (erros === 6) {
-            setImagem(forca6)
+
+        switch (erros){
+            case 0:
+                setImagem(forca0)
+                break
+            case 1:
+                setImagem(forca1)
+                break
+            case 2:
+                setImagem(forca2)
+                break
+            case 3:
+                setImagem(forca3)
+                break
+            case 4:
+                setImagem(forca4)
+                break
+            case 5:
+                setImagem(forca5)
+                break
+            case 6:
+                setImagem(forca6)
+                setStart(false)
+                break
         }
+       
 
         return (<div className="forca">
-            <img src={imagem} alt="" />
+            <img data-identifier="game-image" src={imagem} alt="" />
         </div>)
     }
+    function Palavra(){
 
+        if(isStart){
+            return(<>
+            <p data-identifier="word">{escondido}</p>
+            </>)
+        }else if(isWin){
+            console.log("entrou")
+            return(<>
+            <p className="acertou" data-identifier="word">{palavra}</p>
+            </>)
+        }else if(erros === 6 ){
+            return(<>
+            <p className="perdeu" data-identifier="word">{palavra}</p>
+            </>)
+        } else{
+            return(<>
+            <p></p>
+            </>)
+        }
+    }
     return (
         <div>
             <h1>Jogo da forca</h1>
             <div className='topbar'>
                 <Forca />
                 <div className="palavra">
-                    {!isStart ? <button className='escolher' onClick={comecar}>Escolher palavra</button> : <button className='escolher' disabled>Escolher palavra</button>}
-                    {isStart ? <p>{escondido}</p> : <p></p>}
-                    {clicado}
+                    {!isStart ? <button className='escolher' onClick={comecar} data-identifier="choose-word">Escolher palavra</button> : <button className='escolher' disabled>Escolher palavra</button>}
+                    <Palavra></Palavra>
                 </div>
             </div>
 
             <Botao />
 
             <div className="input">
-                {isStart ? <input type="text" placeholder="Chute a palavra certa" value={chute} onChange={(e) => setChute(e.target.value)} /> : <input type="text" placeholder="Comece o jogo" disabled />}
-                {isStart ? <div className="enviar" onClick={chutarPalavra}><ion-icon name="send"></ion-icon></div> : <div className="enviar-desabilitado"><ion-icon name="send"></ion-icon></div>}
+                {isStart ? <input data-identifier="type-guess" type="text" placeholder="Chute a palavra certa" value={chute} onChange={(e) => setChute(e.target.value)} /> : <input type="text" placeholder="Comece o jogo" disabled />}
+                {isStart ? <div className="enviar" onClick={chutarPalavra} data-identifier="guess-button"><ion-icon name="send"></ion-icon></div> : <button className="enviar-desabilitado" disabled><ion-icon name="send"></ion-icon></button>}
             </div>
         </div>
     )
